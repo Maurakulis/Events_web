5d43c4d165a643fc1aa57d871f46b528dde006ed
dariussh77 <dariussh77@gmail.com>
2023-04-13 21:18:51 +0300

    Anonsai užbaigti

--- scripts/about .js
+++ scripts/about .js
@@ -1,1 +1,13 @@
 import {data} from './data_about.js';
+let qAnonsas=document.querySelector('#anonsas');
+let fAnonsai=()=>{
+    data.forEach((e,i)=>{
+        let timeOut=setTimeout(()=>{
+            //
+            qAnonsas.style.backgroundImage = `url(${e.image_src})`;
+        },i*2000);
+        qAnonsas.addEventListener('mouseover',()=>clearTimeout(timeOut));
+})};    
+
+fAnonsai();
+qAnonsas.addEventListener('click',fAnonsai);
